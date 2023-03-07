287c92c166beb4bd70e271d4d1f79bbb46da22e7
Praharsh Jain <praharshsamria@gmail.com>
2023-03-08 05:17:31 +0530

    added dist-all

--- afterPackHook.js
+++ afterPackHook.js
@@ -4,10 +4,18 @@ const path = require('path');
 exports.default = async function (context) {
     const APP_NAME = context.packager.appInfo.productFilename;
     const APP_OUT_DIR = context.appOutDir;
-    const to = path.join(`${APP_OUT_DIR}`, `${APP_NAME}.app/Contents/Resources/app/package.json`);
-    fs.copyFile("package.json", to, (err) => {
+    const from = "package.json";
+    //path for mac
+    let to = path.join(`${APP_OUT_DIR}`, `${APP_NAME}.app/Contents/Resources/app/package.json`);
+    fs.copyFile(from, to, (err) => {
         if (err) {
-            console.log("\n\n\n error in copying package.json: ", err);
+            //path for windows and linux
+            to = path.join(`${APP_OUT_DIR}`, `/resources/app/package.json`);
+            fs.copyFile(from, to, (err) => {
+                if (err) {
+                    console.log("\n\n\n error in copying package.json: ", err);
+                }
+            });
         }
     });
 }
